aadec6689783bc10e80279ba0f3414759e601651
Markus Unterwaditzer <markus@unterwaditzer.net>
2012-05-15 14:45:24 +0200

    My mods

--- fbdelmsg.user.js
+++ fbdelmsg.user.js
@@ -1,6 +1,7 @@
 // ==UserScript==
 // @name        Facebook: Message quick-delete buttons
-// @description Adds a direct delete button to message threads and replace the "archive" button with a immediate delete button.
+// @description Adds a direct delete button to message threads and replace the 'archive' button with a immediate delete button. Based on http://userscripts.org/scripts/show/106261
+// @version     0.1
 // @namespace   privacyplease
 // @include     http://*.facebook.com/*
 // @include     https://*.facebook.com/*
@@ -10,70 +11,67 @@
 
 // a function that loads jQuery and calls a callback function when jQuery has finished loading
 function addJQuery(callback) {
-	var script = document.createElement("script");
-	script.setAttribute("src", "http://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js");
-	script.addEventListener('load', function() {
-		var script = document.createElement("script");
-		script.textContent = "(" + callback.toString() + ")();";
-		document.body.appendChild(script);
-	}, false);
-	document.body.appendChild(script);
+  var script = document.createElement('script');
+  script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js');
+  script.addEventListener('load', function() {
+    var script = document.createElement('script');
+    script.textContent = '(' + callback.toString() + ')();';
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
 }
 
+
 function switchDeleteButtons() {
-	var q = jQuery;
-	// Restore global $ variable for sanity.
-	$.noConflict();
-	var running = false;
-	var run = function() {
-		if (running) return;
-		if (!document.getElementById("MessagingDashboard")) return;
-		running = true;
-		try {
-		if (document.getElementById("MessagingShelf")) {
-			// console.log("Detected MessagingShelf.");
-			if (!document.getElementById("QuickDelete")) {
-				var actions = q("#MessagingFrame").find('form[class~="uiHeaderActions"]');
-				var tid;
-				actions.find('input[name="tid"]').each(function() {
-					// console.log("Have tid:"+q(this).attr("value"));
-					tid=q(this).attr("value");
-				});
-				if (tid) {
-					var elem = q('<a class="uiButton uiButtonConfirm uiToolbarItem" id="QuickDelete" role="button" rel="dialog"><span class="uiButtonText">Delete All</span></a>');
-					elem.attr("href","/ajax/messaging/async.php?action=deleteDialog&tid="+encodeURIComponent(tid));
-					elem.attr("ajaxify","/ajax/messaging/async.php?action=deleteDialog&tid="+encodeURIComponent(tid));
-					// console.log(elem, actions.find('div[class~="uiToolbarContent"]'));
-					elem.insertBefore(actions.find('div[class~="uiToolbarContent"]').children().children().first());
-				}
-			}
-		} else {
-		// console.log("Looking for archive buttons to replace.");
-		// Avoid false positives by class and structure matching. Better than URLs.
-		q(document).find('li[class~="threadRow"]').each(function() {
-		q(this).find('a[class~="archiveLink"]').each(function() {
-			var a = q(this);
-			var url = a.attr("ajaxify");
-			url = url.replace("action=tag&","action=delete&");
-			a.attr("ajaxify", url);
+  var q = jQuery;
+  // Restore global $ variable for sanity.
+  $.noConflict();
+  var running = false;
+  var run = function() {
+   console.log('Running!');
+    if (running || q('#MessagingDashboard').length == 0) {
+      return;
+    }
+    running = true;
+    try {
+      if (q('#MessagingThreadlist').length == 0) {
+        // Single view of messaging thread
+        console.log('Detected single view');
+        if (q('#QuickDelete').length == 0) {
+          actions = q('#MessagingFrame').find('form.uiHeaderActions');
+          tid = actions.find('input[name=tid]').first().attr('value');
+          if (tid) {
+            elem = q('<a class="uiButton uiButtonConfirm uiToolbarItem" id="QuickDelete" role="button" rel="dialog"><span class="uiButtonText">Delete All</span></a>');
+            elem.attr('href','/ajax/messaging/async.php?action=deleteDialog&tid='+encodeURIComponent(tid));
+            elem.attr('ajaxify','/ajax/messaging/async.php?action=deleteDialog&tid='+encodeURIComponent(tid));
+            // console.log(elem, actions.find('div[class~='uiToolbarContent']'));
+            elem.insertBefore(actions.find('div.uiToolbarContent').children().children().first());
+          }
+        }
+      } else {
+        // We are in the overview of all messages
+        // Avoid false positives by class and structure matching. Better than URLs.
+        console.log('Detected overview');
+        q('li.threadRow a.archiveLink').each(function() {
+          a = q(this);
+          a.attr('ajaxify', a.attr('ajaxify').replace('action=tag&','action=delete&'));
+          a.attr('title', 'Delete instantly');
 
-			a.find('span[class~="uiTooltipText"]').html("DELETE<br /> INSTANTLY");
-			a.find('input').attr("value", "D");
-			var l = a.find('label[class~="uiCloseButton"]');
-			var old = l.attr("class");
-			if (old) {
-				l.attr("class", old.replace("uiCloseButton", "uiDeleteButton"));
-			}
-		});
-		});
-		}
-		}catch(e){console.log("Exception:", e);};
-		running = false;
-	};
+          a.find('input').attr('value', 'D');
+          l = a.find('label.uiCloseButton');
+          if (l.attr('class')) {
+            l.attr('class', 'uiCloseButton uiCloseButtonSmall uiDeleteButton');
+          }
+        });
+      }
+    }
+    catch(e){ console.log('Exception:', e) };
+    running = false;
+  };
 
-	run();
-	jQuery(document).ready(run);
-	jQuery(document).bind("DOMNodeInserted", run);
+  run();
+  jQuery(document).ready(run);
+  jQuery(document).bind('DOMNodeInserted', run);
 }
 
 // load jQuery and execute the main function
