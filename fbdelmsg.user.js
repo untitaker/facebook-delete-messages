57e9c09b5eedddfe903b8629603e1ea1462eb93c
Markus Unterwaditzer <markus@unterwaditzer.net>
2012-05-30 17:29:07 +0200

    Version 0.3
      - Removed jQuery dependency.

--- fbdelmsg.user.js
+++ fbdelmsg.user.js
@@ -3,39 +3,21 @@
 // @description Userscript for Facebook Messages to replace the archive button with a delete button. Based on http://userscripts.org/scripts/show/106261
 // @updateURL   https://github.com/untitaker/facebook-delete-messages/raw/master/fbdelmsg.user.js
 // @downloadURL https://github.com/untitaker/facebook-delete-messages/raw/master/fbdelmsg.user.js
-// @version     0.2
+// @version     0.3
 // @namespace   fbdelmsg
 // @include     http://*.facebook.com/*
 // @include     https://*.facebook.com/*
-// @require     https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js
 // ==/UserScript==
 
-function dump_script(callback) {
+// Inject code from a function into the site
+function dump_script(some_script) {
   script = document.createElement('script');
-  script.textContent = '(' + callback.toString() + ')();';
+  script.textContent = '(' + some_script.toString() + ')();';
   document.body.appendChild(script);
 }
 
 
 function core_script() {
-
-  var load_jquery = function(callback) {
-    var callback_wrapper = function() {
-      $.noConflict(); // This needs to be called ASAP
-      callback();
-    }
-
-    if(typeof jQuery == 'undefined') { // Do we already have jQuery?
-      // This doesn't seem to work the way i expected. I thought Scriptish would install a global jQuery variable,
-      // yet the googleapis version gets downloaded with Scriptish too. FIXME
-      script = document.createElement('script');
-      script.setAttribute('src', 'https://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js');
-      script.addEventListener('load', callback_wrapper);
-      document.body.appendChild(script);
-    } else {
-      callback_wrapper();
-    }
-  }
 
   /*
    * A debug function.
@@ -50,15 +32,25 @@ function core_script() {
   }
 
   /*
-   * Adds all event listeners after jQuery is fully loaded
+   * Adds all event listeners
    * */
   var listen = function() {
-    var q = jQuery;
-  
+
     /*
-    * Variable determines if replace_buttons is running.
-    * */
+     * Variable determines if replace_buttons is running.
+     * */
     var running = false;
+
+    /*
+     * Helper function for loops
+     * */
+    var foreach = function(list, callback) {
+      if(list.length > 0) {
+        for(i=0; i<list.length; i++) {
+          callback(list[i]);
+        }
+      }
+    }
 
     /*
      * Does the main job; replaces all site buttons with the superior ones.
@@ -66,76 +58,100 @@ function core_script() {
      * Facebook's javascript changed something.
      * */
     var replace_buttons = function() {
-      if (running || q('#MessagingDashboard').length == 0) {
+      if (running || !document.getElementById('MessagingDashboard')) {
         return;
       }
       running = true;
-      try {
-        if (q('#MessagingThreadlist').length == 0) {
-          // Single view of messaging thread
-          debug('Detected single view');
-          if (q('#QuickDelete').length == 0) {
-            actions = q('#MessagingFrame').find('.uiHeaderActions');
-            tid = actions.find('input[name=tid]').first().attr('value');
-            if (tid) {
-              elem = q('<a class="uiButton uiButtonConfirm uiToolbarItem" id="QuickDelete" role="button" rel="dialog" title="Delete this conversation"><span class="uiButtonText">Delete</span></a>');
-              elem.attr('href','/ajax/messaging/async.php?action=deleteDialog&tid='+encodeURIComponent(tid));
-              elem.attr('ajaxify','/ajax/messaging/async.php?action=deleteDialog&tid='+encodeURIComponent(tid));
-              elem.insertAfter(actions.find('div.uiToolbarContent > div > .uiToolbarItem:first-child'));
-            }
-          }
-        } else {
-          // We are in the overview of all messages
-          // Avoid false positives by class and structure matching. Better than URLs.
-          debug('Detected overview');
-          if(q('.deleteLink').length == 0) {
-            q('li.threadRow a.archiveLink, li.threadRow a.unarchiveLink').each(function() {
-              a_orig = q(this);
-              a = a_orig.clone(); // Make a deep copy. This will clone the original button and append the clone on it
-              a.attr('ajaxify', a.attr('ajaxify').replace('action=tag&','action=delete&'));
-              a.attr('title', 'Delete this conversation');
-              a.addClass('deleteLink archiveLink').removeClass('unarchiveLink'); // deleteLink for reference, the rest for styling
-              a.css('display', 'block'); // Facebook removes archiveLinks with CSS in archive view
+      if (!document.getElementById('MessagingThreadlist')) {
+        // Single view of messaging thread
+        debug('Detected single view');
+        if (!document.getElementById('QuickDelete')) {
+          actions = document.getElementById('MessagingFrame').getElementsByClassName('uiHeaderActions');
+          tid = document.getElementsByName('tid');
+          if (tid.length > 0) {
+            tid = tid[0].value;
+            toolbar_button = document.createElement('a');
+            toolbar_button.className = 'uiButton uiButtonConfirm uiToolbarItem';
+            toolbar_button.setAttribute('id', 'QuickDelete');
+            toolbar_button.setAttribute('role', 'button');
+            toolbar_button.setAttribute('rel', 'dialog');
+            toolbar_button.setAttribute('title', 'Delete this conversation');
+            toolbar_button.setAttribute('href', '/ajax/messaging/async.php?action=deleteDialog&tid='+encodeURIComponent(tid));
+            toolbar_button.setAttribute('ajaxify', toolbar_button.getAttribute('href'));
 
-              a.find('input').attr('value', 'D');
-              l = a.find('label.uiCloseButton');
-              if (l.attr('class')) {
-                l.attr('class', 'uiCloseButton uiCloseButtonSmall uiDeleteButton');
-              }
-              
-              a.insertAfter(a_orig);
-
-              if(a_orig.hasClass('archiveLink')) {
-                a_orig.remove(); // It is confusing if two buttons have the same icon, so we remove the original button
-              }
-            });
-          }
-
-          // Add "Delete All" button on top
-          if(q('#QuickDelete').length == 0) {
-            toolbar_button = q('<a href="#" class="uiButton uiButtonConfirm uiToolbarItem" id="QuickDelete" role="button" title="Delete all conversations"><span class="uiButtonText">Delete all</span></a>');
-            toolbar_button.click(function(){
-              if(confirm('Do you want to delete all your messages?')) {
-                q('a.deleteLink').each(function(){
-                  console.log(this);
-                  $(this).click(); // I don't know how it works but it does
-                });
-              }
-            });
-            toolbar_button.insertAfter('div.uiToolbarContent > div > .uiToolbarItem:first-child');
+            toolbar_button.innerHTML = '<span class="uiButtonText">Delete</span>';
+            first_toolbar_button = document.getElementsByClassName('uiToolbarItem')[0];
+            first_toolbar_button.parentNode.insertBefore(toolbar_button, first_toolbar_button.nextSibling);
           }
         }
+      } else {
+        // We are in the overview of all messages
+        // Avoid false positives by class and structure matching. Better than URLs.
+        debug('Detected overview');
+        if(document.getElementsByClassName('deleteLink').length == 0) {
+          deal_with_them = function(a_orig) {
+            a = a_orig.cloneNode(true); // Make a deep copy. This will clone the original button
+            a.setAttribute('ajaxify', a.getAttribute('ajaxify').replace('action=tag&','action=delete&'));
+            a.setAttribute('title', 'Delete this conversation');
+            a.className += ' deleteLink archiveLink';
+            a.className = a.className.replace('unarchiveLink', ''); // deleteLink for reference, the rest for styling
+            a.style.display = 'block'; // Facebook removes archiveLinks with CSS in archive view
+
+            foreach(a.getElementsByTagName('input'), function(element) {
+              element.value = 'D';
+            });
+
+            l = a.getElementsByClassName('uiCloseButton')[0];
+            if (l.className) {
+              l.className = 'uiCloseButton uiCloseButtonSmall uiDeleteButton';
+            }
+
+            a_orig.parentNode.insertBefore(a, a_orig.nextSibling);
+
+            if((' '+a_orig.className+' ').replace(' archiveLink ', '') != (' '+a_orig.className+' ')) {
+              // hasClass('archiveLink')
+              a_orig.parentNode.removeChild(a_orig);
+              // It is confusing if two buttons have the same icon, so we remove the original button
+            }
+          }
+
+          // handle the unarchive links before the archive links, otherwise the
+          // already processed unarchive links will get an archive link class
+          // (for style) and will be processed again.
+          foreach(document.getElementsByClassName('archiveLink'), deal_with_them);
+          foreach(document.getElementsByClassName('unarchiveLink'), deal_with_them);
+        }
+
+        // Add "Delete All" button on top
+        if(!document.getElementById('QuickDelete')) {
+          toolbar_button = document.createElement('a');
+          toolbar_button.className = 'uiButton uiButtonConfirm uiToolbarItem';
+          toolbar_button.setAttribute('id', 'QuickDelete');
+          toolbar_button.setAttribute('role', 'button');
+          toolbar_button.setAttribute('title', 'Delete all conversations');
+          toolbar_button.setAttribute('href', '#');
+          toolbar_button.innerHTML = '<span class="uiButtonText">Delete all</span>';
+
+          toolbar_button.addEventListener('click', function(){
+            if(confirm('Do you want to delete all your messages?')) {
+              foreach(document.getElementsByClassName('deleteLink'), function(link){
+                link.click();
+              });
+            }
+          });
+          first_toolbar_button = document.getElementsByClassName('uiToolbarItem')[0];
+          first_toolbar_button.parentNode.insertBefore(toolbar_button, first_toolbar_button.nextSibling);
+
+        }
       }
-      catch(e){ debug('Exception:'+e, 'error') };
       running = false;
     };
 
-    jQuery(document).ready(replace_buttons);
-    jQuery(document).bind('DOMNodeInserted', replace_buttons);
+    document.addEventListener('DOMNodeInserted', replace_buttons);
   }
 
-  // load jquery and add event handlers
-  load_jquery(listen);
+  // add event handlers
+  listen();
 }
 
 // dump the script in the DOM
